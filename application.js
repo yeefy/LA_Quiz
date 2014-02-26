48b2e5fa5565de9b9e2c805aca56ee85693fa108
yeefy <yftran@gmail.com>
2014-02-25 17:57:05 -0800

    update

--- application.js
+++ application.js
@@ -1,16 +1,15 @@
 // JavaScript Document
 
-$(document).ready(function(e) {
-	
 	//each question is an object (includes question, answers, correct) and they're all stored in an array
     var questions = new Array ();
+	var questionIndex = 0;
 	
 	//making the object
 	function questionObject(question, answers, correct){
 		this.question = question;
 		this.answers = answers;
 		this.correct = correct;
-}
+	}
 
 	//array of questions
 	questions [0] = new questionObject ("What does Los Angeles mean in Spanish?", ["The Angels", "The Angles", "The Angry People", "The Ants"], 0);
@@ -18,5 +17,48 @@ $(document).ready(function(e) {
 	questions [2] = new questionObject ("What is UCLA's mascot?",["The Trojans", "The Bruins", "The Angels", "The Bears"], 1);
 	questions [3] = new questionObject ("Which of the following is not a freeway in L.A.?", ["405","10","101","81"], 3);
 	questions [4] = new questionObject ("Which museum in L.A. is known for their lamp posts?", ["MOCA", "LACMA", "Norton Simon", "Bowers"], 1);
+
+
+$(document).ready(function(e) {
 	
+	generateQuestions();
+	generateAnswers();
+
+	
+
+function generateQuestions (){
+	$('#question').append('<h1>' + questions[questionIndex].question + '</h1>');
+	
+}
+
+function generateAnswers () {
+	var answerList = questions[questionIndex].answers;
+	for (var i = 0; i<answerList.length; i++){
+		$('#answers').append("<li><input type='radio' name='answers' value="+ i +">" + answerList[i] +"</li>")
+	};
+	
+
+
+
+$('#next').click(function(e) {
+	var selected = $('input[name="answers"]:checked').val();
+	var correct = questions[questionIndex].correct;
+	if(selected==correct){
+		alert("Correct");
+		$('#question').empty();
+		$('#answers').empty();
+		questionIndex++;
+		generateQuestions();
+		generateAnswers();
+	}else{
+		alert("Wrong");
+		$('#question').empty();
+		$('#answers').empty();
+		questionIndex++;
+		generateQuestions();
+		generateAnswers();
+	}
+});
+};
+
 });
